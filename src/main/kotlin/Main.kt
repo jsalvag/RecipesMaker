e7f2198837e919e52af1e0fed65427bf6a25695b
Jose Salvador Guevara Lunar <jsalvag@gmail.com>
2021-01-19 22:13:33 -0300

    separando las opciones principales del menú en funciones

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,59 +16,75 @@ fun main() {
         val line = readLine()
         when (line?.toInt()) {
             1 -> {
-                val nextRecipe = recipes.size.plus(1)
-                println("\nCreando receta $nextRecipe")
-                val list = mutableListOf<String>()
-                salirCrearReceta@ do {
-                    println("Seleccione un ingrediente")
-                    for ((index, ing) in elements.withIndex()) {
-                        print("${index + 1} - $ing\t")
-                        if (index.plus(1) % 4 == 0) println()
-                    }
-                    println()
-                    if (list.size > 0)
-                        println("${elements.size.plus(1)} - Guardar")
-                    println("0 - Cancelar")
-                    when (val opt = readLine()?.toInt()) {
-                        in 1..elements.size -> {
-                            val selected = elements[opt?.minus(1) ?: 0]
-                            print("\nEscriba cantidad de $selected a agregar: ")
-                            val amount = readLine()?.toInt()
-                            print(
-                                """
-                                    
-                                    Seleccionó $amount de $selected
-                                    Si es correcto presione enter para continuar 
-                                    """.trimIndent()
-                            )
-                            if (readLine().isNullOrBlank()) {
-                                list.add("$selected: $amount")
-                            } else {
-                                continue@salirCrearReceta
-                            }
-                        }
-                        elements.size.plus(1) -> {
-                            println("Recipe $nextRecipe guardado")
-                            recipes.add(list.joinToString(", "))
-                            break@salirCrearReceta
-                        }
-                        0 -> {
-                            println("Recipe $nextRecipe cancelado")
-                            break@salirCrearReceta
-                        }
-                        else -> print("Opción inválida, intente de nuevo: ")
-                    }
-                } while (true)
+                makeRecipe(recipes, elements)
             }
             2 -> {
-                println("Listando mis recetas")
-                println(recipes.joinToString("\n"))
+                viewRecipe(recipes)
             }
             3 -> {
                 println("Adios")
                 break
             }
             else -> println("Opción inválida")
+        }
+    } while (true)
+}
+
+private fun viewRecipe(recipes: MutableList<String>) {
+    if (recipes.size > 0) {
+        println("Listando mis recetas")
+        println(recipes.joinToString("\n"))
+    } else {
+        println("No tienes ninguna receta creada")
+    }
+    readLine()
+}
+
+private fun makeRecipe(
+    recipes: MutableList<String>,
+    elements: List<String>
+) {
+    val nextRecipe = recipes.size.plus(1)
+    println("\nCreando receta $nextRecipe")
+    val list = mutableListOf<String>()
+    salirCrearReceta@ do {
+        println("Seleccione un ingrediente")
+        for ((index, ing) in elements.withIndex()) {
+            print("${index + 1} - $ing\t")
+            if (index.plus(1) % 4 == 0) println()
+        }
+        println()
+        if (list.size > 0)
+            println("${elements.size.plus(1)} - Guardar")
+        println("0 - Cancelar")
+        when (val opt = readLine()?.toInt()) {
+            in 1..elements.size -> {
+                val selected = elements[opt?.minus(1) ?: 0]
+                print("\nEscriba cantidad de $selected a agregar: ")
+                val amount = readLine()?.toInt()
+                print(
+                    """
+                                    
+                                    Seleccionó $amount de $selected
+                                    Si es correcto presione enter para continuar 
+                                    """.trimIndent()
+                )
+                if (readLine().isNullOrBlank()) {
+                    list.add("$selected: $amount")
+                } else {
+                    continue@salirCrearReceta
+                }
+            }
+            elements.size.plus(1) -> {
+                println("Recipe $nextRecipe guardado")
+                recipes.add(list.joinToString(", "))
+                break@salirCrearReceta
+            }
+            0 -> {
+                println("Recipe $nextRecipe cancelado")
+                break@salirCrearReceta
+            }
+            else -> print("Opción inválida, intente de nuevo: ")
         }
     } while (true)
 }
